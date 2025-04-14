396412d546d738dcc46a38a85bca8c69778712d9
gtrmalay <garipov.tat@gmail.com>
2025-04-15 00:47:26 +0300

    First commit

---
+++ Services/MessageLogger.cs
@@ -0,0 +1,23 @@
+﻿using TL;
+
+namespace TgParser.Services
+{
+    public interface IMessageLogger
+    {
+        void LogMessage(Channel channel, Message message);
+    }
+
+    public class MessageLogger : IMessageLogger
+    {
+        public void LogMessage(Channel channel, Message message)
+        {
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Канал: {channel.title}");
+            Console.WriteLine($"ID: {channel.id}");
+            Console.WriteLine($"Создан: {channel.date:dd.MM.yyyy}");
+            Console.WriteLine($"Последнее сообщение ({message.date:dd.MM.yyyy HH:mm}):");
+            Console.WriteLine(message.message);
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
